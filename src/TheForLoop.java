f887ee2e44ddadac24bf9f65ae7809e5758ef7a4
Anis Subba <anis.subba@rockartech.com>
2025-10-03 19:16:39 +0100

    New Lesson forLoop

---
+++ src/TheForLoop.java
@@ -0,0 +1,14 @@
+public class TheForLoop {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        for (count = 1; count <=5; count++){
+            System.out.println(count);
+        }
+    }
+
+
+
+}
