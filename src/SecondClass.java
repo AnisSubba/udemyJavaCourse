c46b190daa4c0b7f46f232f1b0be63bf8264d2f7
Anis Subba <21302329@student.uwl.ac.uk>
2025-12-28 00:16:04 +0000

    Add new comment on this class

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,5 +1,9 @@
 public class SecondClass {
 
+    /**
+     * The main function prints a greeting, checks if a boolean variable is true or false, and then
+     * calls a method to print years and days based on a given number of minutes.
+     */
     public static void main(String[] args){
         System.out.println("Hi Anis");
 
@@ -12,6 +16,16 @@ public class SecondClass {
         System.out.println(printYearsAndDays(1440));
 
     }
+ /**
+  * The function calculates and prints the equivalent number of years and days based on the input
+  * minutes.
+  * 
+  * @param minutes The given code snippet is a Java method that calculates the number of years and days
+  * from a given number of minutes. It first checks if the input minutes is less than 0 and prints
+  * "Invalid Value" if true. Then, it calculates the number of days and years from the minutes input.
+  * @return The method `printYearsAndDays` returns a String that represents the input `minutes`
+  * converted into years and remaining days.
+  */
     public static String printYearsAndDays(long minutes){
 
         if(minutes < 0){
